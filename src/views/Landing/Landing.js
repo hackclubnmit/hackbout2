3dfa7580c2934e85f99e7178136b84af719d1df5
rudyt7 <gallacticsoccer@gmail.com>
2020-10-13 14:54:19 +0530

    landing page responsiveness

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import HackBout from '../../assets/HackBout.png';
-
-import './Landing.css';
+import HackBout from "../../assets/HackBout.svg";
+import "./Landing.css";
 
 const Landing = () => {
   return (
@@ -18,7 +17,7 @@ const Landing = () => {
           <img src={HackBout} alt="hackbout-logo" className="landing__logo" />
         </div>
       </article>
-      <div class="custom-shape-divider-bottom-1600448666">
+      <div className="svg-wave">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -27,7 +26,7 @@ const Landing = () => {
         >
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
+            className="shape-fill"
           ></path>
         </svg>
       </div>
