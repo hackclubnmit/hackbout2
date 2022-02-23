7da5041b0387dd63cb91f6aa022d560a4ffbefd4
SAISHWAR ANAND <saishwaranand32@gmail.com>
2022-02-23 20:37:57 +0530

    added mlh

--- src/views/Mlhform/FormSuccess.js
+++ src/views/Mlhform/FormSuccess.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Form.css';
+import successimg from "../../../src/assets/HackBout.svg"
 
 const FormSuccess = () => {
   return (
     <div className='form-content-right'>
       <h1 className='form-success'>Thankyou..We recieved your form details</h1>
-      <img className='form-img-2' src='img/img-3.svg' alt='success-image' />
+      <img className='form-img-2' src={successimg} alt='success-image' />
     </div>
   );
 };
